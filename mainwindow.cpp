9ddbf3c6721321b889b5e8c961328c4c848553f2
Jakub <jakubwolak.123@gmail.com>
2021-12-27 19:56:37 +0100

    game initialization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,18 +10,43 @@ MainWindow::MainWindow(QWidget *parent)
     // initialize variables
     for (size_t i = 0; i < 3; i++) {
         for (size_t j = 0; j < 3; j++) {
-            board[i][j] = 'A';
-            std::cout << board[i][j] << std::endl;
+            board[i][j] = Ui::Field::None;
         }
     }
     win = false;
     tie = false;
+    can_play = false;
     no_win.reserve(9);
 
     for (size_t i = 0; i < 9; i++) {
         no_win.push_back(0);
-        std::cout << no_win[i] << std::endl;
     }
+
+    buttons[0] = ui->button_1;
+    buttons[1] = ui->button_2;
+    buttons[2] = ui->button_3;
+    buttons[3] = ui->button_4;
+    buttons[4] = ui->button_5;
+    buttons[5] = ui->button_6;
+    buttons[6] = ui->button_7;
+    buttons[7] = ui->button_8;
+    buttons[8] = ui->button_9;
+
+    turn = Ui::Field::None;
+    turn_count = 0;
+
+    // game initialization
+    QObject::connect(ui->start_game, SIGNAL(clicked()), this, SLOT(game_start()));
+
+    QObject::connect(ui->button_1, SIGNAL(pressed()), SLOT(set1()));
+    QObject::connect(ui->button_2, SIGNAL(pressed()), SLOT(set2()));
+    QObject::connect(ui->button_3, SIGNAL(pressed()), SLOT(set3()));
+    QObject::connect(ui->button_4, SIGNAL(pressed()), SLOT(set4()));
+    QObject::connect(ui->button_5, SIGNAL(pressed()), SLOT(set5()));
+    QObject::connect(ui->button_6, SIGNAL(pressed()), SLOT(set6()));
+    QObject::connect(ui->button_7, SIGNAL(pressed()), SLOT(set7()));
+    QObject::connect(ui->button_8, SIGNAL(pressed()), SLOT(set8()));
+    QObject::connect(ui->button_9, SIGNAL(pressed()), SLOT(set9()));
 }
 
 MainWindow::~MainWindow()
@@ -29,3 +54,50 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::game_start() {
+    // choosing player to start game
+    float rand_number = rand();
+    if (rand_number > 0.5) {
+        turn = Ui::Field::X;
+    } else {
+        turn = Ui::Field::O;
+    }
+
+    can_play = true;
+    std::cout << "starting the game" << std::endl;
+}
+
+void MainWindow::set1() { set_turn(1); };
+void MainWindow::set2() { set_turn(2); };
+void MainWindow::set3() { set_turn(3); };
+void MainWindow::set4() { set_turn(4); };
+void MainWindow::set5() { set_turn(5); };
+void MainWindow::set6() { set_turn(6); };
+void MainWindow::set7() { set_turn(7); };
+void MainWindow::set8() { set_turn(8); };
+void MainWindow::set9() { set_turn(9); };
+
+void MainWindow::fill_field(int field_number, Ui::Field field) {
+    if (field == Ui::Field::X) {
+        buttons[field_number - 1]->setText("X");
+    } else if (field == Ui::Field::O) {
+        buttons[field_number - 1]->setText("O");
+    }
+}
+
+void MainWindow::set_turn(int field_number) {
+    if (!can_play) { return; }
+
+    std::cout << field_number << std::endl;
+    const int y = (field_number + 2) % 3;
+    const int x = (field_number - 1) / 3;
+
+    board[x][y] = turn;
+    fill_field(field_number, turn);
+
+    if (turn == Ui::Field::O) {
+        turn = Ui::Field::X;
+    } else {
+        turn = Ui::Field::O;
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,9 +3,14 @@
 
 #include <QMainWindow>
 #include <iostream>
+#include <QPushButton>
+#include <cstdlib>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+    class MainWindow;
+    enum Field { None = ' ', X = 'X', O = 'O'};
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -16,8 +21,32 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void game_start();
+    void set1();
+    void set2();
+    void set3();
+    void set4();
+    void set5();
+    void set6();
+    void set7();
+    void set8();
+    void set9();
+
 private:
     Ui::MainWindow *ui;
-    char board[3][3];
+    Ui::Field board[3][3];
+    QPushButton * buttons[9];
+    bool win;
+    bool tie;
+    std::vector<int> no_win;
+
+    bool can_play;
+
+    Ui::Field turn;
+    int turn_count;
+
+    void set_turn(int field_number);
+    void fill_field(int field_number, Ui::Field field);
 };
 #endif // MAINWINDOW_H
